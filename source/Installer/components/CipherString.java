03f9e0b90e892e88a4cd0b9699b767af0406c2d1
dpalic <darko.palic@xenovation.com>
2014-09-13 02:47:57 +0200

    added

---
+++ source/Installer/components/CipherString.java
@@ -0,0 +1,174 @@
+package components;
+
+/*
+ * $Id: Encoder.java,v 1.1 2007/04/20 19:37:20 paujones2005 Exp $ $Name:  $
+ *
+ * Copyright  2004, 2005, 2006  www.jrivet.com
+ * 
+ *   @author P. Jones  
+ * 	 @version 2.060719
+ *
+ * The contents of this file are subject to the Mozilla Public License Version 1.1
+ * (the "License"); you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS" basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+ * for the specific language governing rights and limitations under the License.
+ *
+ * The Original Code is 'jRivet Framework - Java Solutions for Enterprise Applications'.
+ *
+ * The Initial Developer of the Original Code is Paul Jones. Portions created by
+ *  the Initial Developer are Copyright (C) 2004, 2005, 2006  by Paul Jones.
+ *
+ *  **All Rights Reserved **.
+ *
+ * Contributor(s): all the names of the contributors are added in the source code
+ * where applicable.
+ *
+ * Alternatively, the contents of this file may be used under the terms of the
+ * LGPL license (the "GNU LIBRARY GENERAL PUBLIC LICENSE"), in which case the
+ * provisions of LGPL are applicable instead of those above.  If you wish to
+ * allow use of your version of this file only under the terms of the LGPL
+ * License and not to allow others to use your version of this file under
+ * the MPL, indicate your decision by deleting the provisions above and
+ * replace them with the notice and other provisions required by the LGPL.
+ * If you do not delete the provisions above, a recipient may use your version
+ * of this file under either the MPL or the GNU LIBRARY GENERAL PUBLIC LICENSE.
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the MPL as stated above or under the terms of the GNU
+ * Library General Public License as published by the Free Software Foundation;
+ * either version 2 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Library general Public License for more
+ * details.
+ *
+ * If you didn't download this code from the following link, you should check if
+ * you aren't using an obsolete version:
+ *
+ * http://www.jRivet.com/download/
+ */
+
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.KeySpec;
+
+import javax.crypto.Cipher;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.DESKeySpec;
+import javax.crypto.spec.DESedeKeySpec;
+
+import sun.misc.BASE64Decoder;
+import sun.misc.BASE64Encoder;
+
+public class CipherString {
+
+	public static final String DESEDE_SCHEME = "DESede";
+	public static final String DES_SCHEME = "DES";
+	public static final String DEFAULT_KEY =  
+		"oSvSGrkOPdwIkSlxhpPzfKJFFrPAyConOnEaUvWHqwvjePGrAUzpbROJiZnEaXAuqiQUDREa";
+	
+	private KeySpec keySpec;
+	private SecretKeyFactory keyFactory;
+	private Cipher cipher;
+	private static final String UNICODE_FORMAT = "UTF8";
+
+	public CipherString() throws EncryptionException {
+		this(DESEDE_SCHEME, DEFAULT_KEY);
+	}
+		
+	
+	public CipherString(String key) throws EncryptionException {
+		this(DESEDE_SCHEME, key);
+	}
+
+	public CipherString(String encryptionScheme, String encryptionKey)  {
+
+		if (encryptionKey == null)
+			encryptionScheme = DEFAULT_KEY;
+		if (encryptionKey.trim().length() < 24)
+			encryptionScheme = DEFAULT_KEY;
+
+		try {
+			byte[] keyAsBytes = encryptionKey.getBytes(UNICODE_FORMAT);
+
+			if (encryptionScheme.equals(DESEDE_SCHEME)) {
+				keySpec = new DESedeKeySpec(keyAsBytes);
+			} else if (encryptionScheme.equals(DES_SCHEME)) {
+				keySpec = new DESKeySpec(keyAsBytes);
+			} else {
+				throw new IllegalArgumentException("Encryption scheme not supported: " + encryptionScheme);
+			}
+
+			keyFactory = SecretKeyFactory.getInstance(encryptionScheme);
+			cipher = Cipher.getInstance(encryptionScheme);
+
+		} catch (InvalidKeyException e) {
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		} catch (NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public String encrypt(String str) throws EncryptionException {
+		if (str == null || str.trim().length() == 0)
+			return null;
+
+		try {
+			SecretKey key = keyFactory.generateSecret(keySpec);
+			cipher.init(Cipher.ENCRYPT_MODE, key);
+			byte[] cleartext = str.getBytes(UNICODE_FORMAT);
+			byte[] ciphertext = cipher.doFinal(cleartext);
+
+			BASE64Encoder base64encoder = new BASE64Encoder();
+			return base64encoder.encode(ciphertext);
+		} catch (Exception e) {
+			throw new EncryptionException(e);
+		}
+	}
+
+	public String decrypt(String str) throws EncryptionException {
+		if (str == null || str.trim().length() <= 0)
+			return null;
+
+		try {
+			SecretKey key = keyFactory.generateSecret(keySpec);
+			cipher.init(Cipher.DECRYPT_MODE, key);
+			BASE64Decoder base64decoder = new BASE64Decoder();
+			byte[] cleartext = base64decoder.decodeBuffer(str);
+			byte[] ciphertext = cipher.doFinal(cleartext);
+
+			return bytes2String(ciphertext);
+		} catch (Exception e) {
+			throw new EncryptionException(e);
+		}
+	}
+
+	private static String bytes2String(byte[] bytes) {
+		StringBuffer stringBuffer = new StringBuffer();
+		for (int i = 0; i < bytes.length; i++) {
+			stringBuffer.append((char) bytes[i]);
+		}
+		return stringBuffer.toString();
+	}
+
+	public static class EncryptionException extends Exception {
+
+		private static final long serialVersionUID = 1L;
+
+		public EncryptionException(Throwable t) {
+			super(t);
+		}
+	}
+}
